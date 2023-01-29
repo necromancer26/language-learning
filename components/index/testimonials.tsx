74b0ab7ca7065f62f8df1a73053b2d6e6d4e3adb
Khalil <khalil.chettaoui06@gmail.com>
2023-01-29 12:43:33 +0100

    fixing

--- components/index/testimonials.tsx
+++ components/index/testimonials.tsx
@@ -28,7 +28,7 @@ export default function Testimonials() {
             <blockquote className="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">
               <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
                 <svg
-                  class="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600"
+                  className="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600"
                   viewBox="0 0 24 27"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
@@ -41,7 +41,7 @@ export default function Testimonials() {
                 Speechless with how easy this was to integrate
               </h3>
               <p className="my-4">
-                "I recently got my hands on Flowbite Pro, and holy crap, I'm
+                I recently got my hands on Flowbite Pro, and holy crap, I'm
                 speechless with how easy this was to integrate within my
                 application. Most templates are a pain, code is scattered, and
                 near impossible to theme.
@@ -75,7 +75,7 @@ export default function Testimonials() {
                 Solid foundation for any project
               </h3>
               <p className="my-4">
-                "FlowBite provides a robust set of design tokens and components
+                FlowBite provides a robust set of design tokens and components
                 based on the popular Tailwind CSS framework. From the most used
                 UI components like forms and navigation bars to the whole app
                 screens designed both for desktop and mobile, this UI kit
@@ -83,7 +83,7 @@ export default function Testimonials() {
               </p>
               <p className="my-4">
                 Designing with Figma components that can be easily translated to
-                the utility classNamees of Tailwind CSS is a huge timesaver!"
+                the utility classNamees of Tailwind CSS is a huge timesaver!
               </p>
             </blockquote>
             <figcaption className="flex justify-center items-center space-x-3">
@@ -106,9 +106,9 @@ export default function Testimonials() {
                 Mindblowing workflow and variants
               </h3>
               <p className="my-4">
-                "As someone who mainly designs in the browser, I've been a
-                casual user of Figma, but as soon as I saw and started playing
-                with FlowBite my mind was 🤯.
+                As someone who mainly designs in the browser, I've been a casual
+                user of Figma, but as soon as I saw and started playing with
+                FlowBite my mind was 🤯.
               </p>
               <p className="my-4">
                 Everything is so well structured and simple to use (I've learnt
@@ -116,7 +116,7 @@ export default function Testimonials() {
               </p>
               <p className="my-4">
                 Aesthetically, the well designed components are beautiful and
-                will undoubtedly level up your next application."
+                will undoubtedly level up your next application.
               </p>
             </blockquote>
             <figcaption className="flex justify-center items-center space-x-3">
@@ -139,13 +139,13 @@ export default function Testimonials() {
                 Efficient Collaborating
               </h3>
               <p className="my-4">
-                "This is a very complex and beautiful set of elements. Under the
+                This is a very complex and beautiful set of elements. Under the
                 hood it comes with the best things from 2 different worlds:
                 Figma and Tailwind.
               </p>
               <p className="my-4">
                 You have many examples that can be used to create a fast
-                prototype for your team."
+                prototype for your team.
               </p>
             </blockquote>
             <figcaption className="flex justify-center items-center space-x-3">
