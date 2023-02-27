8443a5c94ca4b9fe021487e7c445309e9cba4e10
Khalil <khalil.chettaoui06@gmail.com>
2023-02-28 00:41:01 +0100

    icon

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -1,9 +1,5 @@
-import  { useState } from 'react';
-// import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-
 export default function schedule() {
-    const [value, onChange] = useState(new Date());
+
   return (
     <div className="h-[100vh] bg-slate-300 flex flex-col justify-center items-center font-[Inter]">
       <h1>Schedule your appointement</h1>
