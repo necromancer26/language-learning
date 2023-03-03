d440ad464a7b05e004ec5afe8f2d59655cd2ea9d
Khalil <khalil.chettaoui06@gmail.com>
2023-03-04 00:45:55 +0100

    icons removal

--- components/reusables/title-description.tsx
+++ components/reusables/title-description.tsx
@@ -13,13 +13,13 @@ export default function TiteDescription({
       <div className=" grid grid-cols-1 gap-6 my-6 md:grid-cols-2 ">
         <div className="">
           <div className="flex gap-6 ">
-            <Image
+            {/* <Image
               height={30}
               width={30}
               alt="earth icon"
               src="https://polydioms.fra1.cdn.digitaloceanspaces.com/assets%2Ficons%2Fearth.png"
               // src="https://polydioms.fra1.cdn.digitaloceanspaces.com/assets%2Ficons%2Fplanet-earth.png"
-            />
+            /> */}
           </div>
           <span className="mb-5 text-m font-bold  font-[spectral]  transition-all">
             {description}
@@ -27,12 +27,12 @@ export default function TiteDescription({
         </div>
         <div>
           <div className="flex gap-6">
-            <Image
+            {/* <Image
               height={30}
               width={30}
               alt="earth icon"
               src="https://polydioms.fra1.cdn.digitaloceanspaces.com/assets%2Ficons%2Fwork-from-home.png"
-            />
+            /> */}
           </div>
           <span className="mb-5 text-m font-bold  font-[spectral]  transition-all">
             {description}
@@ -40,12 +40,12 @@ export default function TiteDescription({
         </div>
         <div>
           <div className="flex gap-6">
-            <Image
+            {/* <Image
               height={30}
               width={30}
               alt="earth icon"
               src="https://polydioms.fra1.cdn.digitaloceanspaces.com/assets%2Ficons%2Fmeeting.png"
-            />
+            /> */}
           </div>
           <span className="mb-5 text-m font-bold  font-[spectral]  transition-all">
             {description}
@@ -53,12 +53,12 @@ export default function TiteDescription({
         </div>{" "}
         <div>
           <div className="flex gap-6">
-            <Image
+            {/* <Image
               height={30}
               width={30}
               alt="earth icon"
               src="https://polydioms.fra1.cdn.digitaloceanspaces.com/assets%2Ficons%2Fbrain.png"
-            />
+            /> */}
           </div>
           <span className="mb-5 text-m font-bold  font-[spectral]  transition-all">
             {description}
