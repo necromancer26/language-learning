c2e27516aae2b4955ce8acba9236e8f4e3fe4a67
Khalil <khalil.chettaoui06@gmail.com>
2023-03-16 22:20:53 +0100

    updates

--- components/courses/courses-list.tsx
+++ components/courses/courses-list.tsx
@@ -3,12 +3,19 @@ import { useEffect, useState } from "react";
 import CourseList from "../../interfaces/Courses";
 import ServicesSection from "../index/services-section";
 import BasicCard from "../reusables/basic-card";
-export default function CoursesList({ courses }: any) {
+export default function CoursesList({ courses, courseListToShow }: any) {
+  useEffect(() => {
+    return () => {};
+  }, [courseListToShow]);
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 md:grid-cols-2">
-      {courses?.map((course: any, idx: number) => (
-        <BasicCard course={course} key={idx} idx={idx} />
-      ))}
+      {courses?.map(
+        (course: any, idx: number) =>
+          course[courseListToShow] && (
+            <BasicCard course={course} key={idx} idx={idx} />
+          )
+      )}
     </div>
   );
 }
--- pages/online-classes.tsx
+++ pages/online-classes.tsx
@@ -8,26 +8,39 @@ interface Props {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 const OnlineClasses: React.FC<Props> = ({ courses }) => {
-  const [coursesToList, setCoursesToList] = useState("isOnline");
+  const [courseListToShow, setCourseListToShow] = useState("isOnline");
   return (
     <div className="bg-slate-100">
       <Header
         title="Online classes to learn your preferred language"
         description="Learn the language you want with the guidance of our expert tutors"
-      />
-      {/* <div className=" bg-slate-100 w-full flex flex-col justify-center items-center gap-6 pt-6 font-[playfairdisplay] "></div> */}
+      />  
       <div className="flex justify-center items-center bg-transparent my-10">
-        <div className=" bg-slate-100 text-slate-900 lg:w-96 flex justify-center items-center border-2 border-slate-900 rounded-full">
-          <button className="py-3 text-center focus:text-slate-50 focus:bg-slate-900 flex justify-center items-center  w-full rounded-full">
+        <div className=" bg-slate-100 text-slate-900 lg:w-[400px] w-[80%] flex justify-center items-center border-2 border-slate-900 rounded-full">
+          <button
+            className={`h-full py-3 text-center  ${
+              courseListToShow === "isOnline"
+                ? "bg-slate-900 text-slate-50"
+                : "bg-transparent"
+            }  flex justify-center items-center  w-full rounded-full`}
+            onClick={() => setCourseListToShow("isOnline")}
+          >
             Online courses
           </button>
-          <button className="py-3 text-center focus:text-slate-50 focus:bg-slate-900 flex justify-center items-center w-full rounded-full">
+          <button
+            className={`h-full py-3 text-center  ${
+              courseListToShow === "isInPerson"
+                ? "bg-slate-900 text-slate-50"
+                : "bg-transparent"
+            }  flex justify-center items-center  w-full rounded-full`}
+            onClick={() => setCourseListToShow("isInPerson")}
+          >
             In-person courses
           </button>
         </div>
       </div>
       <section className="flex w-full justify-center items-center py-6 bg-slate-100">
-        <CoursesList courses={courses} />
+        <CoursesList courses={courses} courseListToShow={courseListToShow} />
       </section>
     </div>
   );
